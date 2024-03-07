05113bd466a0473e9459de379f169f882d04adde
trangmai10 <trangmai14102002.nd@gmail.com>
2024-03-07 20:25:01 +0700

    tinh tong

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,10 +1,18 @@
 internal class Program {
     private static void Main(string[]args)
     {
-    string str="123";
-    int a= Convert.ToInt32(str);
+        int a,b; 
+    system.Console.Write("a=");
+     a= Convert.ToInt32(Console.ReadLine());
+    system.Console.Write("b=");
+     b= Convert.ToInt32(Console.ReadLine());
+     //in tổng a, b
+     system.Console.WriteLine("{0}+{1}={2}",a,b,a+b);
+     }
+     }
+    
 
-    system.Console.WriteLine("a="+a);
+    
 
    
     }
