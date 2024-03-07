310377f84120a6e9e1777721304c106bc477bd20
trangmai10 <trangmai14102002.nd@gmail.com>
2024-03-07 20:01:42 +0700

    chuyen doi kieu dl

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,15 +1,14 @@
 internal class Program {
     private static void Main(string[]args)
     {
-    Console.Write("nhạp dl vào :");
-    int a=Cononsole.Read();
-    Console.WriteLine("dl nhập vào :"+a);
-    string hoten= "mai thị thu trang";
-    int tuoi="18";
-    Console.WriteLine("sinh vien{0}-{1} tuổi ",hoten,tuoi);
+    int a=10;
+    float b=a; 
+    system.Console.WriteLine("b="+b);
+
+   
     }
     }
-    }
+    
 
 
 
