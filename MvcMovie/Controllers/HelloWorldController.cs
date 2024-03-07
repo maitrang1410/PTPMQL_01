3d6664e595e14118471be69568b7344ac156c50d
trangmai10 <trangmai14102002.nd@gmail.com>
2024-03-07 18:09:29 +0700

    Helloworld.mvc

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+namespace MvcMovie.Controllers{
+    public class HelloWorldController: Controller {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
+
+        }
+
+        }
