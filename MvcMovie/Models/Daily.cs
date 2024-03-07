ceb004d3522ec4b4a3cc3c1286d8d4dcfee9e83b
trangmai10 <trangmai14102002.nd@gmail.com>
2024-03-07 19:06:05 +0700

    models

---
+++ MvcMovie/Models/Daily.cs
@@ -0,0 +1,16 @@
+namespace MvcMovie.Models;
+public class Daily : Hethongphanphoi{
+    public string MaDaiLy {get;set}
+    public string TenDaiLy {get;set}
+    public string DiaChi {get;set}
+    public string NguoiDaiDien {get;set}
+    public string DienThoai {get;set}
+    public string MaHTPP {get;set}
+        }
+
+    
+
+
+
+
+}
---
+++ MvcMovie/Models/Hethongphanphoi.cs
@@ -0,0 +1,5 @@
+namespace MvcMovie.Models;
+public class Hethongphanphoi{
+    public string MaHTPP{get;set}
+    public string TenHTPP{get;set}
+}
