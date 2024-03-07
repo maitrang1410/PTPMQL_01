8ee94449d4b66b82b35d84ef7efb9818f2c9de5a
trangmai10 <trangmai14102002.nd@gmail.com>
2024-03-07 21:08:17 +0700

    lenh continue

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,7 +3,7 @@ public class Program {
     private static void Main(string[]args)
     {
     for( int i=0; i<10;i++)
-    {if(i==5)break;
+    {if(i==5) continue;
     system.Cosole.WriteLine(" vòng lặp thứ {0}",i)
  }
  }
