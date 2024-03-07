ea29dc8f0bbcb25c4e3e7a7d495ffb9e7077b3f7
trangmai10 <trangmai14102002.nd@gmail.com>
2024-03-07 20:39:09 +0700

    toan tu cong, tru, nhan, chia , thuong

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace Programa{
+namespace Program{
 public class Program {
     private static void Main(string[]args)
     class ToanTu{
