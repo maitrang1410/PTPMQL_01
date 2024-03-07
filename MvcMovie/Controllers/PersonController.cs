047d12b80ffd6e89e777cd96da28ad9cb3e6b0ae
trangmai10 <trangmai14102002.nd@gmail.com>
2024-03-07 19:24:26 +0700

    personcontroll

--- MvcMovie/Controllers/PersonController.cs
+++ MvcMovie/Controllers/PersonController.cs
@@ -4,5 +4,9 @@ public class PersonController : Controller {
         return View();
         }
    
-    
+    [httpPost]
+    public IActionResult Index(Peson ps){
+        string StrOut= "xin chao"+ps.PersonId+ "-"+ps.FullName+"-"+ps.Address;
+        ViewBag.infoPerson= strOutput;
+        return View();
             }
