de81516712b6853ed054509712fafc65f8052626
trangmai10 <trangmai14102002.nd@gmail.com>
2024-03-07 21:32:24 +0700

    class_phuong thuc

--- NewApp/Models/Person.cs
+++ NewApp/Models/Person.cs
@@ -2,6 +2,14 @@ namespace NewApp.Models;
 
 public class Person
 {
-    public void MyMethod(string parameter){
-        //câu lệnh pthuc
+    public void EnterData(){
+    System.Cosnsole.Write("Full name=");
+    FullName= Console.ReadLine();
+    System.Cosnsole.Write("Full name=");
+    Address= Console.ReadLine();
+
+}
+public void Display(){
+    System.Cosnsole.WriteLine("{0}-{1}",FullName, Address);
+}
 }
