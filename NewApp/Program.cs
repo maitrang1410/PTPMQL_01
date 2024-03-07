659c50728cabb8ab803341ae47cd8cf262f2d403
trangmai10 <trangmai14102002.nd@gmail.com>
2024-03-07 20:51:00 +0700

    cau lenh if..else

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,36 +1,23 @@
 
-using System.Threading.Tasks;
-namespace Program{
 public class Program {
     private static void Main(string[]args)
-    class ToanTusosanh{
     {
-        int a,b; 
-    system.Console.Write("a=");
-     a= Convert.ToInt32(Console.ReadLine());
-    system.Console.Write("b=");
-     b= Convert.ToInt32(Console.ReadLine());
-     //in tổng a, b
+ float a =10;
+    if(a >= 8){
+        System.Console.WriteLine("Hoc sinh hoc luc gioi");
+    }else if(a >= 6.5){
+        System.Console.WriteLine("Hoc sinh hoc luc kha");
+    }else if(a >= 5){
+        System.Console.WriteLine("Hoc sinh hoc luc trung binh");
+    }else if(a >= 3.5){
+        System.Console.WriteLine("Hoc sinh hoc luc yeu");
+    }else{
+        System.Console.WriteLine("Hoc sinh hoc luc kem");
+    }
+    }
+}
 
-        if (a<b)
-        {
-            Console.WriteLine($"{a}<{b}");
-        }else if(a==b){
-        
-            Console.WriteLine($"{a}={b}");
-     
 
-
-     }else
-     {
-            Console.WriteLine($"{a}>{b}");
-        
-     }
-     }
-     }
-     }
-     }
-     
      
      
     
